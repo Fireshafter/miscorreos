3c8e67213ea5fb3cfc8446106c33bb2f64845711
Fireshafter <fireshafter@hotmail.com>
2020-02-11 01:21:39 +0100

    se ha añadido el componente navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  nuevoMensaje: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  abrir(){
+    this.nuevoMensaje=true;    
+  }
+
+  cerrar(){
+    this.nuevoMensaje=false;
+  }
+
+}
